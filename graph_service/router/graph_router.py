c20b7f18aab05f44d1225f301a3f35a0bb380554
JohnLavender474 <johnmichaellavender123@gmail.com>
2026-01-29 21:54:54 -0500

    Add repo for graph invocations

--- graph_service/router/graph_router.py
+++ graph_service/router/graph_router.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 
 from fastapi import APIRouter
 
@@ -11,6 +12,13 @@ from langchain_core.messages import (
 from graph import build_graph
 from model.graph_input import GraphInput
 from model.graph_state import GraphState
+from model.graph_invocation import (
+    GraphInvocation,
+    GraphInvocationState,
+)
+from repository.graph_invocation_repository import (
+    GraphInvocationsRepository
+)
 
 from logging import logger
 
@@ -18,6 +26,9 @@ from logging import logger
 logger = logger.getLogger(__name__)
 
 router = APIRouter(prefix="/api/graph", tags=["graph"])
+
+
+invocations_repo = GraphInvocationsRepository()
 
 
 @router.post(
@@ -29,36 +40,62 @@ async def invoke_graph(
 ) -> GraphState:
     logger.debug(
         f"Graph invocation requested for query: " 
-        f"{json.dumps(input_data.user_query, indent=2)}"
+        f"{json.dumps(input_data, indent=2)}"
     )
 
-    graph_state_messages: list[BaseMessage] = []
-
-    for message in input_data.messages:
-        if message.role == "user":
-            graph_state_messages.append(
-                HumanMessage(content=message.content)
-            )
-        elif message.role == "ai":
-            graph_state_messages.append(
-                AIMessage(content=message.content)
-            )
-        else:
-            raise Exception(f"Invalid message: {message}")
-
-    state = GraphState(
-        user_query=input_data.user_query,
-        profile_id=input_data.profile_id,
-        messages=graph_state_messages,
+    invocation_id = str(uuid.uuid4())
+    invocation = GraphInvocation(
+        invocation_id=invocation_id,        
     )
-    
-    graph = build_graph()
+    invocations_repo.put_invocation(invocation)
 
-    result = await graph.ainvoke(state)
+    logger.debug(f"Graph invocation id: {invocation_id}")
 
-    logger.debug(
-        f"Graph execution completed with result: " 
-        f"{json.dumps(result, indent=2)}"
-    )
-    
-    return result
+    try:
+        graph_state_messages: list[BaseMessage] = []
+
+        for message in input_data.messages:
+            if message.role == "user":
+                graph_state_messages.append(
+                    HumanMessage(content=message.content)
+                )
+            elif message.role == "ai":
+                graph_state_messages.append(
+                    AIMessage(content=message.content)
+                )
+            else:
+                raise Exception(f"Invalid message: {message}")
+
+        state = GraphState(
+            user_query=input_data.user_query,
+            profile_id=input_data.profile_id,
+            messages=graph_state_messages,
+        )
+        
+        graph = build_graph()
+
+        result = await graph.ainvoke(state)
+
+        logger.debug(
+            f"Graph execution completed with result: " 
+            f"{json.dumps(result, indent=2)}"
+        )
+
+        invocation.invocation_state = (
+            GraphInvocationState.COMPLETED
+        )
+        invocation.graph_state = result
+        invocations_repo.put_invocation(invocation)
+        
+        return result
+    except Exception as e:
+        logger.error(
+            f"Graph invocation {invocation_id} failed: {str(e)}"
+        )
+
+        invocation.invocation_state = (
+            GraphInvocationState.FAILED
+        )
+        invocations_repo.put_invocation(invocation)
+        
+        raise e
